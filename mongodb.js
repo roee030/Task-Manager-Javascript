e17a4b4b02ba8a4f8ece0e1cd2dd195f67fc5e58
roee030 <roee.angel93@gmail.com>
2020-04-23 17:03:56 +0300

    creating monogo server

---
+++ mongodb.js
@@ -0,0 +1,8 @@
+//CRUD
+
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient // help me to get more function for mongodb
+
+const connectionURL = 'mongodb://127.0.0.1:27017'
+const database = ''
+
