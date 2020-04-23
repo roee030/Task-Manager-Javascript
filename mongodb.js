0d3b95ae5808d2051e92f928c936ec848f2eddbe
roee030 <roee.angel93@gmail.com>
2020-04-23 17:17:06 +0300

    insert first document

--- mongodb.js
+++ mongodb.js
@@ -4,5 +4,21 @@ const mongodb = require('mongodb')
 const MongoClient = mongodb.MongoClient // help me to get more function for mongodb
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
-const database = ''
+const databaseName = 'task-manager'
+
+MongoClient.connect(connectionURL, {useNewUrlParser:true, useUnifiedTopology: true}, (error , client) => {
+    if (error){
+        return console.log('Unable to connect to database');
+        
+    }
+
+    const db = client.db(databaseName)
+    
+    db.collection('users').insertOne({
+        name: 'Roee',
+        age:"26"
+    })
+    
+    
+})
 
