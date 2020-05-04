6d2368e00236eda37ee4bb3023fd5553885d2d81
roee030 <roee.angel93@gmail.com>
2020-05-04 14:07:37 +0300

    replace callback to async/await

--- src/index.js
+++ src/index.js
@@ -8,61 +8,82 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 
-app.post('/users', (req, res) =>{
+app.post('/users', async (req, res) =>{
     const user = new User(req.body)
-    user.save().then(()=>{
-        res.send('Success to save user')
-    }).catch(()=>{
+    try{
+        await user.save()
+        res.status(201).send('The user save successfully')
+    }catch{
         res.send('Failed to save user')
-    })
+    }
 })
 
-app.get('/users', (req, res) => {
-    User.find({}).then((users) =>{
+
+app.get('/users', async (req, res) => {
+    try{
+        const users = await User.find({})
+        if(!users){
+            res.send('There is no users to shown')
+        }
         res.send(users)
-    }).catch((e)=>{
-        res.send(e)
-    })
+    }catch{
+        res.status(500).send()
+    }
 })
 
 
-app.get('/users/:id', (req, res) => {
-   const _id = req.params.id
-   User.findById(_id).then((user) => {
-       if(!user) return res.send('There is no user with id '+ _id)
-       res.send(user)
-   }).catch((e) =>{
-       return res.send(e)
-   })
-})
-
-app.post('/tasks', (req, res) =>{
-    const task = new Task(req.body)
-    task.save().then(()=>{
-        res.send('Success to save task')
-    }).catch(() => {
-        res.send('Failed to save task')
-    })
-})
-
-
-app.get('/tasks' , (req, res) => {
-    Task.find({}).then((task) =>{
-        res.send(task)
-    }).catch((e) => {
-        res.send(e)
-    })
-})
-
-app.get('/tasks/:id' , (req, res) => {
+app.get('/users/:id', async(req, res) => {
     const _id = req.params.id
-    Task.findById(_id).then((task) => {
-        if(!task) return res.send('There is no task with this id')
-        res.send(task)
-    }).catch((e) => {
-        return res.send('There is no task with this id')
-    })
+    try{
+        const user = await User.findById(_id)
+        if(!user){
+            res.send('There is no user with this id')
+        }
+        res.send(user)
+    }catch{
+        res.status(500).send('There is no user with this id')
+    }
 })
+
+
+app.post('/tasks', async(req, res) =>{
+   const task = req.body
+   try{
+       await Task.save(task)
+       res.send('The task save successfully')
+   }catch{
+       res.status(500).send('Could not save the task')
+   }
+})
+
+
+app.get('/tasks' , async (req, res) => {
+    try{
+        const tasks = await Task.find({})
+        if(!tasks){
+            res.send('There is no tasks')
+        }
+        res.send(tasks)
+    }catch{
+        res.status(500).send('Could not find tasks')
+    }
+
+})
+
+
+app.get('/tasks/:id' , async(req, res) => {
+   const id = req.params.id
+   try{
+       const task = await Task.findById(id)
+       if(!task){
+           res.send('There is no task with this id')
+       }
+   }catch{
+       res.status(500).send('Could not find task with this id')
+   }
+})
+
+
 
 app.listen(port, () => {
     console.log('server is on port '+ port);
